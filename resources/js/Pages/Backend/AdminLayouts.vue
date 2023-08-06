809be9260f53fdac1eb24044058c0c83a19d5645
Mohammad alamin <mohammadalamin.dev@gmail.com>
2023-08-06 21:10:44 +0600

    mastering admin panel

---
+++ resources/js/Pages/Backend/AdminLayouts.vue
@@ -0,0 +1,130 @@
+<template >
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+        </ul>
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+        
+            <!-- Notifications Dropdown Menu -->
+            <li class="nav-item dropdown">
+                <a class="nav-link" data-bs-toggle="dropdown" href="#">
+                    <i class="far fa-bell"></i>
+                    <span class="badge badge-warning navbar-badge">15</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                    <span class="dropdown-item dropdown-header">15 Notifications</span>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <i class="fas fa-envelope mr-2"></i> 4 new messages
+                        <span class="float-right text-muted text-sm">3 mins</span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <i class="fas fa-users mr-2"></i> 8 friend requests
+                        <span class="float-right text-muted text-sm">12 hours</span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <i class="fas fa-file mr-2"></i> 3 new reports
+                        <span class="float-right text-muted text-sm">2 days</span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                </div>
+            </li>
+            <!-- Profile Dropdown Menu -->
+            <li class="nav-item dropdown user-menu show">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true">
+                    <img :src="logo" class="user-image img-circle elevation-2" alt="User Image">
+                    <span class="d-none d-md-inline"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
+                    <!-- User image -->
+                    <li class="user-header bg-success">
+                        <img :src="logo" class="img-circle elevation-2" alt="User Image">
+                        <p>
+
+                            <small>Web Developer</small>
+                        </p>
+                    </li>
+                    <!-- Menu Footer-->
+                    <li class="user-footer">
+                        <a href="" class="btn btn-secondary">Profile</a>
+                        <a href="" class="btn btn-danger float-right">Sign out</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                    <i class="fas fa-expand-arrows-alt"></i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-light-primary elevation-4">
+        <!-- Brand Logo -->
+        <a href="#" class="brand-link">
+            <img :src="logo" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">Admin</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar Menu -->
+            <nav class="mt-3">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-header">Site Settings</li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-file"></i>
+                            <p>Manage Banner</p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-12 ">
+                        <slot name="headerTitle"></slot>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <slot></slot>
+    </div>
+    <section class="container-fluid">
+        
+    </section>
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2023 <a href="" ></a></strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Mohammad Alamin</b>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            logo: 'uploads/images/logo.png'
+        }
+    },
+}
+</script>
+
+
+<style></style>
--- resources/js/Pages/Backend/Dashboard.vue
+++ resources/js/Pages/Backend/Dashboard.vue
@@ -2,16 +2,23 @@
     <Head title="Dashboard">
         <!-- <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> -->
     </Head>
-    <div>
-        <p>Technology Used:</p>
-    </div>
+    <AdminLayouts>
+        <template v-slot:headerTitle>
+            <h1 class="m-0">Dashboard</h1>
+        </template>
+
+        <template v-slot:defautl>
+            <p>Technology Used:</p>
+        </template>
+    </AdminLayouts>
 </template>
 <script>
 
-import { Head } from '@inertiajs/vue3'
+import { Head } from '@inertiajs/vue3';
+import AdminLayouts from './AdminLayouts.vue';
 
 export default {
-    components: {Head }
+    components: {Head,AdminLayouts }
 }
 </script>
 <style scoped></style>
